ec218f99c912e8e9293551d43eed2aae70947419
ANANYAQA-creator <ananyrawe@gmail.com>
2025-06-05 22:33:24 +0530

    New Insert Lab001 and Lab002 added

---
+++ src/ex_01_Java_Basic_V1/Lab001_HelloWorld_V1.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic_V1;
+
+public class Lab001_HelloWorld_V1 {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld, starting my JAVA journey");
+    }
+}
---
+++ src/ex_01_Java_Basic_V1/Lab002_Addition_V1.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic_V1;
+
+public class Lab002_Addition_V1 {
+    public static void main(String[] args) {
+
+        System.out.println(2+3);
+    }
+
+
+}
