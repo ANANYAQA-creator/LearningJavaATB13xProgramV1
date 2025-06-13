d7cb626854a14ac18c4aa2179a98c47272d8ad06
ANANYAQA-creator <ananyrawe@gmail.com>
2025-06-13 12:45:42 +0530

    10th june java program V1

---
+++ src/ex_03_Java_Literals/Lab021_Java_NonPrimitive_Literals.java
@@ -0,0 +1,26 @@
+package ex_03_Java_Literals;
+
+public class Lab021_Java_NonPrimitive_Literals {
+    public static void main(String[] args) {
+
+        // Primitive Datatypes  :- defined by the JAVA guys
+        //Max, Min, Size defined
+        byte b = 20; // 1Byte = 8 bits
+        int a = 10;  // 4 Byte = 32 bits
+
+        //Non-Primitive Data types (Defined By Users) ,  Reference Data Types
+
+        // Defined by User
+        // No size, max, min.
+        // Byte-8 , Bits:-64
+
+        String name = "Ananya" ; // String is a bunch of char.
+        int[] arrays_of_items = new int[20] ;
+
+        // String,Arrays,Classes,enum..
+
+        String s = null ;  //:- String can be null
+        // int a = null ;  // int never null
+
+    }
+}
