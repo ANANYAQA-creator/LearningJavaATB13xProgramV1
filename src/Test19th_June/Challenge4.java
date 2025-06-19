aede391c133a5c16dfcdaa368c78d38bbfd9803f
ANANYAQA-creator <ananyrawe@gmail.com>
2025-06-19 08:36:37 +0530

    Challenge4

---
+++ src/Test19th_June/Challenge4.java
@@ -0,0 +1,15 @@
+package Test19th_June;
+
+public class Challenge4 {
+    public static void main(String[] args) {
+
+        int i = 5;
+      //  System.out.println(i);  // original 5
+        System.out.println(++i); //  pre incre  6
+        System.out.println(i++); //  post incre 6
+
+        int i1 =6;
+        System.out.println(--i1); //  pre decre  5
+        System.out.println(i1--); // post decre  5
+    }
+}
