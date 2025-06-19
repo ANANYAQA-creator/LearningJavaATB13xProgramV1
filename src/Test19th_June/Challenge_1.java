96158310cfb34bd65a1669e511cb214fe0bafcb2
ANANYAQA-creator <ananyrawe@gmail.com>
2025-06-19 08:24:18 +0530

    Challenge

---
+++ src/Test19th_June/Challenge_1.java
@@ -0,0 +1,19 @@
+package Test19th_June;
+
+public class Challenge_1 {
+    public static void main(String[] args) {
+
+int a= 25;
+        System.out.println(a);   // 25
+
+        double pie = 3.14;
+        System.out.println(pie);  //3.14
+
+        boolean B = true ;
+        System.out.println(B);  // true
+
+        char c = 'A';
+        System.out.println(c);  // A
+
+    }
+}
