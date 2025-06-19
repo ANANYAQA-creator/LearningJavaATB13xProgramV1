0ff15fe53c38dd52413aaf3c681bf28d00489e7a
ANANYAQA-creator <ananyrawe@gmail.com>
2025-06-19 08:28:33 +0530

    Challenge2

---
+++ src/Test19th_June/Challenge_2.java
@@ -0,0 +1,15 @@
+package Test19th_June;
+
+public class Challenge_2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a+b);   // Addition - 15
+        System.out.println(a-b);   // Substraction - 5
+        System.out.println(a*b);   // multi - 50
+        System.out.println(a/b);   // division - 2
+        System.out.println(a%b);   // modulus - 0
+    }
+}
